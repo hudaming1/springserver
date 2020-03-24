e6ba997f34840eae11218e95ebf2c7cf8c08cf4f
hudaming1 <512657752@qq.com>
2020-03-24 18:31:11 +0800

    修改代码

--- src/main/java/org/hum/springserver/controller/work/ExpressController.java
+++ src/main/java/org/hum/springserver/controller/work/ExpressController.java
@@ -20,8 +20,79 @@ public class ExpressController {
 
 	@RequestMapping("/zto/callback")
 	@ResponseBody
-	public Object user(HttpServletRequest request) {
+	public Object ztoCallback(HttpServletRequest request) {
 		logger.info("zto.callback");
+		if (!request.getParameterMap().isEmpty()) {
+			request.getParameterMap().entrySet().forEach(p -> {
+				logger.info(p.getKey() + "=" + Arrays.toString(p.getValue()));
+			});
+		}
+		JSONObject result = new JSONObject();
+		result.put("status", true);
+		return result;
+	}
+
+	@RequestMapping("/yt/callback")
+	@ResponseBody
+	public Object ytCallback(HttpServletRequest request) {
+		logger.info("yt.callback");
+		if (!request.getParameterMap().isEmpty()) {
+			request.getParameterMap().entrySet().forEach(p -> {
+				logger.info(p.getKey() + "=" + Arrays.toString(p.getValue()));
+			});
+		}
+		JSONObject result = new JSONObject();
+		result.put("status", true);
+		return result;
+	}
+
+	@RequestMapping("/sto/callback")
+	@ResponseBody
+	public Object stoCallback(HttpServletRequest request) {
+		logger.info("sto.callback");
+		if (!request.getParameterMap().isEmpty()) {
+			request.getParameterMap().entrySet().forEach(p -> {
+				logger.info(p.getKey() + "=" + Arrays.toString(p.getValue()));
+			});
+		}
+		JSONObject result = new JSONObject();
+		result.put("status", true);
+		return result;
+	}
+
+	@RequestMapping("/zjs/callback")
+	@ResponseBody
+	public Object zjsCallback(HttpServletRequest request) {
+		logger.info("yt.callback");
+		if (!request.getParameterMap().isEmpty()) {
+			request.getParameterMap().entrySet().forEach(p -> {
+				logger.info(p.getKey() + "=" + Arrays.toString(p.getValue()));
+			});
+		}
+		JSONObject result = new JSONObject();
+		result.put("status", true);
+		return result;
+	}
+
+	@RequestMapping("/bskd/callback")
+	@ResponseBody
+	public Object bskdCallback(HttpServletRequest request) {
+		logger.info("bskd.callback");
+		if (!request.getParameterMap().isEmpty()) {
+			request.getParameterMap().entrySet().forEach(p -> {
+				logger.info(p.getKey() + "=" + Arrays.toString(p.getValue()));
+			});
+		}
+		JSONObject result = new JSONObject();
+		result.put("status", true);
+		return result;
+	}
+
+
+	@RequestMapping("/kd100/callback")
+	@ResponseBody
+	public Object kd100Callback(HttpServletRequest request) {
+		logger.info("kd100.callback");
 		if (!request.getParameterMap().isEmpty()) {
 			request.getParameterMap().entrySet().forEach(p -> {
 				logger.info(p.getKey() + "=" + Arrays.toString(p.getValue()));
